b872f5ce510d06d128299c89c218ca6ca75f5f31
INTEL <yousufzubair1234@gmail.com>
2022-11-07 18:26:03 +0500

    Completed the first challenge of keeper app

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+
+function App() {
+    return (
+        <div>
+            <Header></Header>
+            <Note></Note>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const date = new Date;
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>Copyright © {date.getFullYear()}</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
