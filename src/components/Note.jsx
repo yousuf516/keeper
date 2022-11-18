de02b686b234fa93d83ebe8daf15e12ccd3deb9f
INTEL <yousufzubair1234@gmail.com>
2022-11-19 00:21:14 +0500

    Completed the challenge all by myself

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Note(props) {
+  function handleClick(event) {
+    props.onDelete(props.id);
+    event.preventDefault();
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>DELETE</button>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
